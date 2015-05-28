2d5afd7ff0aa6c58d6d68fedc71b4667c5e8f454
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-05-28 08:23:26 +0200

    migrated to Revit 2016

--- BipChecker/Properties/AssemblyInfo.cs
+++ BipChecker/Properties/AssemblyInfo.cs
@@ -32,5 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+//
+// 2015-05-28 2016.0.0.0 migrated to Revit 2016
+//
 [assembly: AssemblyVersion("2016.0.0.0")]
 [assembly: AssemblyFileVersion("2016.0.0.0")]
