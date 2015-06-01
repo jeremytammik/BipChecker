07e2f12f072be9f5bef8ce4d7724eb440a46ae68
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-06-01 12:23:09 +0200

    added potential todo item

--- BipChecker/Properties/AssemblyInfo.cs
+++ BipChecker/Properties/AssemblyInfo.cs
@@ -22,6 +22,10 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid( "118f7279-630d-4661-afe5-c23c23acf46f" )]
 
+// Todo:
+// Make use of the Revit 2016 Parameter API additions?
+// http://thebuildingcoder.typepad.com/blog/2015/04/whats-new-in-the-revit-2016-api.html#5.02
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
